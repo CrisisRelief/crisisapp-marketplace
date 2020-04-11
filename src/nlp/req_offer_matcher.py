d4cb7676a525d4c3973940793a16cfc26e68a6e9
supertramped <cganjihal@deloitte.com.au>
2020-04-11 23:01:47 +1000

    match other cats

--- src/nlp/req_offer_matcher.py
+++ src/nlp/req_offer_matcher.py
@@ -3,6 +3,7 @@ from typing import List, Dict
 
 from src.common import req_offer_template as req_offer_cfg
 from src.common.fricles_logger import *
+from src.nlp.doc_sim import document_similarity as doc_sim
 
 class match_request(object):
 
@@ -12,6 +13,8 @@ class match_request(object):
             Country Type defaults to Australia. Update the config file to modify
             '''
             self.geocode_dist = pgeocode.GeoDistance(country_type)
+            self.ds = doc_sim()
+            
         except Exception as e:
             fricles_logger.warning('Failed to load geocode info for ' + \
                                    str(country_type) + "with error code: " + str(e))
@@ -30,10 +33,16 @@ class match_request(object):
         Returns:
             score (float): Match score
         '''
-        score = 0
+        
+        threshold = req_offer_cfg.DEFAULT_SIM_MATCH_THRESHOLD
+        if type(req_details) != str or type(offer_details) != str:
+            return 0
+        
+        score = self.ds.get_sent2vec_similarity(req_details.lower(), offer_details.lower())
+
         return score
 
-    def match_req_with_offer(self, req:req_offer_cfg, offer:req_offer_cfg) -> float:
+    def match_req_with_offer(self, req:req_offer_cfg, offer:req_offer_cfg, nlp_match_only:bool = False) -> float:
         '''
         Match a specific request with offer based on the fields present as part of request/offer datastructure
         This function takes in only one req and offer so that it can be a parallelized operation
@@ -48,20 +57,31 @@ class match_request(object):
             score (float): match score between 0 and 1
 
         '''
+        scores = {}
         score = 0
+
+        nlp_match_score = self.get_nlp_match_score(req['req_offer_details'], offer['req_offer_details'])
+        scores['nlp_match_score'] = nlp_match_score
+        
+        if nlp_match_only:
+            return nlp_match_score
+        
+        score *= 1 + nlp_match_score
+        
         if req['crisis_name'] != offer['crisis_name']:
+            scores['crisis_match_score'] = 0
             return 0
         else:
+            scores['crisis_match_score'] = 1
             score += 1
-
+            
         deg_overlap = len(set(req['req_offer_cat']).intersection(set(offer['req_offer_cat'])))
         if not deg_overlap:
+            scores['req_offer_category_match_score'] = 0
             return 0
-        
-        score += deg_overlap
 
-        nlp_score = self.get_nlp_match_score(req['req_offer_details'], offer['req_offer_details'])
-        score *= 1 + nlp_score
+        scores['req_offer_category_match_score'] = deg_overlap
+        score += deg_overlap
 
         if 'derived_attrs' in req and not req['derived_attrs']['online_request']:
             try:
@@ -69,22 +89,33 @@ class match_request(object):
             except Exception as e:
                 fricles_logger.warning('Failed to get distance between post codes: ' + str(e))
                 dist = -1
-
+                scores['distance_score'] = -1
+        else:
+            scores['distance_score'] = -1
+                
         '''if distance is greater than threshold the score will be very low 
         '''
         if dist >= 0:
             norm_dist = dist/req_offer_cfg.DEFAULT_DISTANCE_THRESHOLD
-
+            scores['distance_score'] = dist
+            scores['normalised_distance_score'] = norm_dist
+            
             if norm_dist > 1:
                 fricles_logger.info('Distance greater than threshold')
             else:
                 score *= 1 + (1 - norm_dist)
 
+        else:
+            scores['distance_score'] = -1
+            scores['normalised_distance_score'] = -1
+            
         fricles_logger.info('Match score: ' + str(score))
 
-        return score
+        scores['overall_score'] = score
+        
+        return scores
 
-    def match_reqs_with_offers(self, reqs:List, offers:List) -> List[Dict]:
+    def match_reqs_with_offers(self, reqs:List, offers:List, nlp_match_only:bool = False) -> List[Dict]:
         '''
         Match a list of requests and offers, best suited for distributed execution
         Args:
@@ -97,13 +128,32 @@ class match_request(object):
         matches = []
         for req in reqs:
             for offer in offers:
-                score = self.match_req_with_offer(req, offer)
-                if score:
-                    match = {}
-                    match['score'] = score
-                    match['request'] = req
-                    match['offer'] = offer
-                    matches.append(match)
+                scores = self.match_req_with_offer(req, offer, nlp_match_only)
+                match = {}
+                match['overall_score'] = scores['overall_score']
+                match['request_serial_number'] = req['serial_number']
+                match['offer_serial_number'] = offer['serial_number']
+                match['request_category'] = req['req_offer_cat']
+                match['offer_category'] = offer['req_offer_cat']
+                match['request_description'] = req['req_offer_details']
+                match['offer_description'] = offer['req_offer_details']
+                match['request_email'] = req['email']
+                match['offer_email'] = offer['email']
+                match['request_phone_number'] = req['phone_number']
+                match['offer_phone_number'] = offer['phone_number']
+                match['request_suburb'] = req['loc_suburb']
+                match['offer_suburb'] = offer['loc_suburb']
+                match['request_post_code'] = req['loc_post_code']
+                match['offer_post_code'] = offer['loc_post_code']
+                match['request_state'] = req['loc_state']
+                match['offer_state'] = offer['loc_state']
+                
+                match['nlp_match_score'] = scores['nlp_match_score']
+                match['distance_score'] = scores['distance_score']
+                match['normalised_distance_score'] = scores['normalised_distance_score']
+                match['req_offer_category_match_score'] = scores['req_offer_category_match_score']
+                match['crisis_match_score'] = scores['crisis_match_score']
+                matches.append(match)
                     
 
         return matches
--- test/test_req_offer_matches.py
+++ test/test_req_offer_matches.py
@@ -3,7 +3,7 @@ import copy
 import src.common.req_offer_template as req_offer_cfg
 
 
-f = "test/request_offer_test_file.csv"
+f = "test/request_offer_help.csv"
 
 def read_test_file(f):
 
@@ -14,6 +14,8 @@ def read_test_file(f):
     print(df.shape)
 
     for index, row in df.iterrows():
+
+        sub_type_req = False
         if row['Crisis name'] != "COVID-19 pandemic":
             continue
         sub = copy.deepcopy(req_offer_cfg.req_offer)
@@ -30,18 +32,25 @@ def read_test_file(f):
         sub['full_name'] = row['Full name']
         cats = row['Request/offer category']
         list_of_cats = cats.split(";")
-        if len(list_of_cats) == 1 and 'Other - please detail' in row['Request/offer category']:
+        
+        #if len(list_of_cats) == 1 and 'Other - please detail' in row['Request/offer category']:
+        #    continue
+
+        if not 'Other - please detail' in row['Request/offer category']:
             continue
+        
+        sub['serial_number'] = row['Serial number']
         sub['req_offer_cat'] = list_of_cats
         sub['req_offer_details'] = row['Detailed description']
         sub['email'] = row['Your email']
         sub['phone_number'] = row['Your phone number']
         sub['loc_st_addr'] = row['Your location: Address']
         sub['loc_suburb'] = row['Your location: City/Town']
+        
         '''Entry does not have valid state
         '''
-        if row['Your location: State'] == 'Select State':
-            continue
+        #if row['Your location: State'] == 'Select State':
+        #    continue
         sub['loc_state'] = row['Your location: State']
         sub['loc_post_code'] = row['Your location: Postcode']
 
@@ -53,11 +62,14 @@ def read_test_file(f):
     return reqs, offers
 
 r, o = read_test_file(f)
-print(len(r))
-print(len(o))
 
 from src.nlp.req_offer_matcher import match_request
 
 mr = match_request()
-matches = mr.match_reqs_with_offers(r, o)
-print("matches")
+matches = mr.match_reqs_with_offers(r, o, nlp_match_only=False)
+
+op = pd.DataFrame(matches)
+op.to_cs("match_outputs.csv", index = False)
+
+
+
